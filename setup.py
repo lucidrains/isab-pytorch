2f99c8678978adee74b243348636ae477a663de1
Phil Wang <lucidrains@gmail.com>
2020-10-26 14:16:41 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'isab-pytorch',
+  packages = find_packages(),
+  version = '0.0.1',
+  license='MIT',
+  description = 'Induced Set Attention Block - Pytorch',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  url = 'https://github.com/lucidrains/isab-pytorch',
+  keywords = [
+    'artificial intelligence',
+    'attention mechanism'
+  ],
+  install_requires=[
+    'torch',
+    'einops>=0.3'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
